310e9312c75bd79e18d7846fb7de75904d784ac8
wherearetheavocados <avocados@linuxmail.org>
2021-01-31 17:57:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/dereference23/positron_miatoll -b staging kernel --depth=5 --recurse-submodules
+git clone https://github.com/AnggaR96s/msm-4.14 -b eleven kernel --depth=5 --recurse-submodules
 cd kernel
 curl https://raw.githubusercontent.com/bagus02/DroneCI/main/build.sh | bash
