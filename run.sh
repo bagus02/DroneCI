fcb56adadbcd3c77c949df1746aea952e17f6c9d
wherearetheavocados <avocados@linuxmail.org>
2021-02-02 07:28:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/gengkapak/kernel_xiaomi_sm6250 -b eleven kernel --depth=5 --recurse-submodules
+git clone https://git.rip/ga/kernel_xiaomi_miatoll kernel --depth=5
 cd kernel
 curl https://raw.githubusercontent.com/bagus02/DroneCI/main/build.sh | bash
